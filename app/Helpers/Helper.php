614c5f5713e37df44030bf00c9049152db7e488f
Do Minh Vu <dominhvu278@gmail.com>
2022-05-02 22:11:26 +0700

    intall themes and menu

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -2,6 +2,7 @@
 
 
 namespace App\Helpers;
+use Illuminate\Support\Str;
 
 
 class Helper
@@ -38,5 +39,39 @@ class Helper
 
     public static function active($active = 0) : string  {
         return $active == 0 ? '<span class="btn btn-danger btn-xs">NO</span>' : '<span class="btn btn-success btn-xs">YES</span>';
+    }
+
+
+    public static function menus($menus, $parent_id = 0)
+    {
+        $html = '';
+        foreach ($menus as $key => $menu) {
+            if ($menu->parent_id == $parent_id) {
+                $html .= '
+                    <li>
+                        <a href="/danh-muc/'. $menu->id . '-' . Str::slug($menu->name, '-') .'.html" >
+                            '. $menu->name .'
+                        </a>';
+
+                if (self::isChild($menus, $menu->id)) {
+                    $html .= '<ul class="sub-menu">';
+                    $html .= self::menus($menus, $menu->id);
+                    $html .= '</ul>';
+                }
+
+                   $html .=' </li>
+                ';
+            }
+        }
+        return $html;
+    }
+
+    public static function isChild($menus, $id) {
+        foreach ($menus as $menu) {
+            if ($menu->parent_id == $id) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,6 @@ use App\Http\Controllers\Admin\UploadController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Admin\Users\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('admin/users/login', [LoginController::class, 'index'])->name('login');
 Route::post('admin/users/login/store', [LoginController::class, 'store']);
@@ -68,4 +54,4 @@ Route::middleware(['auth'])->group(function () {
     });
 });
 
-
+Route::get('/', [\App\Http\Controllers\User\MainController::class, 'index']);
