2b2d689203b2e831dc5e7826c6715b4e9c1ac533
Do Minh Vu <dominhvu278@gmail.com>
2022-05-02 18:50:52 +0700

    CRUD slider

---
+++ app/Http/Services/Slider/SliderService.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace App\Http\Services\Slider;
+
+
+use App\Models\Slider;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
+
+class SliderService
+{
+    public function insert($request) {
+        try {
+            Slider::create($request->input());
+            Session::flash('success', 'Thêm slider thành công');
+
+        }catch (\Exception $err) {
+            Session::flash('error', 'Thêm slider lỗi');
+            \Log::info($err->getMessage());
+
+            return false;
+        }
+        return true;
+    }
+
+    public function get()
+    {
+        return Slider::orderByDesc('id')->paginate(15);
+    }
+
+    public function update(Request $request, Slider $slider)
+    {
+
+        try {
+            $slider->fill($request->input());
+            $slider->save();
+            Session::flash('success', 'Cập nhật danh mục thành công');
+        } catch (\Exception $err) {
+            Session::flash('error', 'Cập nhật slider lỗi');
+            Log::info($err->getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public function destroy(Request $request)
+    {
+        $slider = Slider::where('id', $request->input('id'))->first();
+        if ($slider) {
+            $path = str_replace('storage', 'public', $slider->thumb);
+            Storage::delete($path);
+            $slider->delete();
+            return true;
+        }
+        return false;
+    }
+}
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -72,6 +72,29 @@
                         </li>
                     </ul>
                 </li>
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-images"></i>
+                        <p>
+                            Slider
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{URL::to('/admin/sliders/add')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Thêm slider</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{URL::to('/admin/sliders/list')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Danh sách slider</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
 
             </ul>
         </nav>
